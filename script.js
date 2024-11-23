989c23b60ce7c72b7a426a672dd7e55578291593
YanchukViktoria <139624726+YanchukViktoria@users.noreply.github.com>
2024-11-23 15:36:25 +0200

    fs

--- script.js
+++ script.js
@@ -8,6 +8,18 @@
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
+    refs.modal.addEventListener("click", event => {
+      if (event.currentTarget === event.target) {
+        refs.modal.classList.add("is-hidden");
+      }
+    });
+  
+    document.addEventListener("keydown", event => {
+      if (event.key === "Escape") {
+        refs.modal.classList.add("is-hidden");
+      }
+      });
+  
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
     }
